e404778456774101861fe26f93d5e9561febc925
z <alex@dwfe.ru>
2019-11-24 02:19:17 +0300

    .

--- src/App/Main/Main.tsx
+++ src/App/Main/Main.tsx
@@ -6,14 +6,13 @@ export const Main: React.FC = (props) => {
     <main className="main">
       <div className="app__adaptive-container">
         <div className="main__content">
-          Данные
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur, earum. Doloremque, necessitatibus perferendis perspiciatis velit vero voluptate voluptatum! Accusantium delectus deserunt, dolor exercitationem id, maiores molestias necessitatibus neque nihil nisi, officiis perspiciatis placeat qui ratione rem rerum sapiente sunt tempora tempore temporibus tenetur veritatis voluptate voluptatibus. Ab accusamus at cupiditate dolorem doloribus eius error, exercitationem explicabo fugiat laudantium
-            magnam
-            minima mollitia pariatur qui quidem quos repellat temporibus voluptatum! Adipisci amet autem blanditiis, corporis cumque dicta dolor doloribus ducimus exercitationem explicabo facilis ipsum iure iusto labore numquam obcaecati omnis, quae qui quis ratione recusandae reprehenderit rerum sint soluta sunt temporibus, ullam! Aliquam consectetur debitis dolores earum eius eum, explicabo illum inventore ipsa iusto mollitia nulla, numquam officiis quo reprehenderit repudiandae sed, sit soluta ullam voluptas? Dolorem
-            pariatur porro sed voluptate? Alias assumenda consequuntur debitis deserunt distinctio dolore dolores ea error ex excepturi explicabo id illum ipsa iste iure magnam minima nobis, nostrum odio odit, officia optio perferendis quis reiciendis repellendus sequi sint suscipit tenetur veniam voluptatum. Architecto atque doloremque eligendi excepturi impedit iure minus natus neque officiis omnis, perspiciatis placeat quos recusandae sequi, sit tenetur veniam veritatis. Dolores magnam, porro quaerat quas similique sint
-            ut?
-            Culpa laborum mollitia porro ullam?
-          </p>
+            <h2>Welcome!</h2>
+            <p>Do|While|For|Each is a composite application.</p>
+            <p><a href="https://gitlab.com/Do-While-For-Each/backend" target="_blank" rel="noopener noreferrer">Backend</a> is written in Java using microservice architecture.</p>
+            <p>As a frontend you can use anything that can communicate with the backend API.</p>
+            <p>While there is only two <a href="https://gitlab.com/Do-While-For-Each/frontend" target="_blank" rel="noopener noreferrer">frontend implementation</a> and it is made on React. You are now interacting with this implementation.</p>
+            <p>--</p>
+            <p>src: <a href="https://gitlab.com/Do-While-For-Each" target="_blank" rel="noopener noreferrer">gitlab.com/Do-While-For-Each</a></p>
         </div>
       </div>
     </main>
