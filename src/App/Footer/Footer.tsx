26c86759eeaea835a5dae65b2bdb2ae632c2da01
z <alex@dwfe.ru>
2019-11-23 21:54:25 +0300

    .

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -4,8 +4,13 @@ import './Footer.less'
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <div className="footer__copyright">Copiright, год</div>
-      <div className="footer__lang">Язык</div>
+      <div className="app__adaptive-container">
+        <div className="footer__copyright">Copiright, год</div>
+        <div className="footer__middle">
+          middle
+        </div>
+        <div className="footer__lang">Язык</div>
+      </div>
     </footer>
   );
 };
